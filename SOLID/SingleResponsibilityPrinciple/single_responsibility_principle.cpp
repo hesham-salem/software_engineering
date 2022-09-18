949877a95f3f0dfbbab19e7a0cf363b2e7df65cd
Hesham Salem <hesham1salem@gmail.com>
2022-09-18 11:05:58 +0200

    rename original example add new example

--- SOLID/SingleResponsibilityPrinciple/single_responsibility_principle.cpp
+++ SOLID/SingleResponsibilityPrinciple/single_responsibility_principle.cpp
@@ -1,10 +1,17 @@
+
+//The Single Responsibility Principle (SRP)
+//The idea behind the SRP is that every class, module, or function in a program should have one responsibility/purpose in a program. 
+//As a commonly used definition, "every class should have only one reason to change".
+
+
+
 /*
 Consider a class called XMLExporter. XMLExporter takes a Document object,
 and exports it into a different file format for another application.
 
 
 
-class XMLExporter {
+class Exporter {
 private:
     URL _runSaveDialog();
     String _exportDocumentToXML(Document doc);
@@ -13,7 +20,7 @@ public:
     void exportDocument(Document doc);
 };
 
-void XMLExporter::exportDocument(Document doc)
+void Exporter::exportDocument(Document doc)
 {
     String xmlFileContent = _exportDocumentToXML(doc);
     URL fileURL = _runSaveDialog();
@@ -35,7 +42,7 @@ public:
 
 //  XMLExporter
 
-class XMLExporter {
+class Exporter {
 private:
     URL _runSaveDialog();
     void _showSuccessDialog();
@@ -43,7 +50,7 @@ public:
     void exportDocument(Document doc);
 };
 
-void XMLExporter::exportDocument(Document doc)
+void Exporter::exportDocument(Document doc)
 {
     XMLConverter converter;
     String xmlFileContent = converter.convertDocumentToXML(doc);
@@ -58,3 +65,48 @@ int main()
 {
 
 }
+
+
+
+public class Student {
+
+     public void registerStudent() {
+         // some logic
+     }
+
+     public void calculate_Student_Results() {
+         // some logic
+     }
+
+     public void sendEmail() {
+         // some logic
+     }
+
+}
+//The class above violates the single responsibility principle. Why?
+
+//This Student class has three responsibilities – registering students, calculating their results, and sending out emails to students.
+
+//The code above will work perfectly but will lead to some challenges. We cannot make this code reusable for other classes or objects. The class has a whole lot of logic interconnected that we would have a hard time fixing errors. And as the codebase grows, so does the logic, making it even harder to understand what is going on.
+
+//Imagine a new developer joining a team with this sort of logic with a codebase of about two thousand lines of code all congested into one class.
+
+//Now let's fix this!
+
+public class StudentRegister {
+    public void registerStudent() {
+        // some logic
+    }
+}
+public class StudentResult {
+    public void calculate_Student_Result() {
+        // some logic
+    }
+}
+public class StudentEmails {
+    public void sendEmail() {
+        // some logic
+    }
+}
+
+
