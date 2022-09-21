37434e493f9a736dc4d73ebe026909a45e7447f4
Hesham Salem <hesham1salem@gmail.com>
2022-09-21 11:09:48 +0200

    comment

--- SOLID/DependencyInjection/dependency_inversion_principle.cpp
+++ SOLID/DependencyInjection/dependency_inversion_principle.cpp
@@ -1,5 +1,5 @@
 /*https://www.geeksforgeeks.org/dependecy-inversion-principle-solid/*/
-
+// dependency inversion is solid principle while Dependency Injection is design pattern apply this principle
 
 int main()
 {
